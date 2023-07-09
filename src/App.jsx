ff04e3bd124f4add0ccf3e941af053c14e16ae48
Dharmesh-gohil <dharmesh.vatsalgohil@gmail.com>
2023-07-09 06:53:16 +0530

    successfully done all functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,27 @@ import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
 import Values from "values.js";
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
-  const [colors,setColors]=useState(new Values("#f15025").all(10))
+  const [colors, setColors] = useState(new Values("#f15025").all(10))
+  // toast.success("successfully done")
+  // toast.error("error")
+
+  const addColor = (color) => { 
+    // console.log(color)
+  try {
+    const newColor = new Values(color).all(10)
+    setColors(newColor)
+  } catch (error) {
+    toast.error(error.message)
+  }
+  }
+
   return <main>
-    <Form />
-    <ColorList colors={ colors} />
+    <Form addColor={ addColor} />
+    <ColorList colors={colors} />
+    <ToastContainer position="top-center"/>
   </main>;
 };
 export default App;
--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,9 +1,28 @@
+import { toast } from "react-toastify"
+
 const SingleColor = ({ index, color }) => {
     // console.log(color)
     const {weight, hex}=color
 
+    const saveToClipboard = async () => { 
+        if (navigator.clipboard) {
+            try {
+                await navigator.clipboard.writeText(`#${hex}`)
+                toast.success("successfully color copied  to clipboard")
+                
+            } catch (error) {
+                toast.error("failed to  copy color to clipboard")
+            }
+
+        } else { 
+            toast.error("clipboard not available in browser setting so do change init")
+        }
+    }
+
   return (
-      <article className={ index>10?"color color-light":"color"} style={{ background: `#${hex}` }}>
+      <article className={index > 10 ? "color color-light" : "color"}
+          style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}>
       
           <p className="percent-value">{weight}%</p>
           <p className="color-value">#{ hex}</p>
